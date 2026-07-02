0b2e7a37f86aafca9e121549056cd9e16daf7699
Tyriq Williams <tywilliams001@yahoo.com>
2026-07-02 11:29:43 -0400

    Fixed typo in temp_conversion.c

--- temp_conversion.c
+++ temp_conversion.c
@@ -13,7 +13,7 @@ int main() {
 
     fahr = lower;
     while (fahr <= upper) {
-        celsius = (5.0/9.0) * (fahr - 32);
+        celsius = (5.0/9.0) * (fahr - 32.0);
         printf("%3.0f\t%6.1f\n", fahr, celsius);
 
         fahr = fahr + step;
