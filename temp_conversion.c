126697acfa285f2b595d7bd4a9838fbce73b6307
Tyriq Williams <tywilliams001@yahoo.com>
2026-07-02 11:28:03 -0400

    temp conversion improvements

--- temp_conversion.c
+++ temp_conversion.c
@@ -4,7 +4,7 @@
    for fahr = 0, 20, ..., 300 */
 
 int main() {
-    int fahr, celsius;
+    float fahr, celsius;
     int lower, upper, step;
 
     lower = 0;      /* lower limit of temperature table */
@@ -13,8 +13,9 @@ int main() {
 
     fahr = lower;
     while (fahr <= upper) {
-        celsius = 5 * (fahr - 32) / 9;
-        printf("%d\t%d\n", fahr, celsius);
+        celsius = (5.0/9.0) * (fahr - 32);
+        printf("%3.0f\t%6.1f\n", fahr, celsius);
+
         fahr = fahr + step;
     }
 }
